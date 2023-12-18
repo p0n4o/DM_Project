6172977a478e5a6ac2c949fedd27487f7d0903e4
Yurakov Nazar <ngyurakov_1@edu.hse.ru>
2023-12-18 15:47:34 +0300

    4th select added

--- Select.sql
+++ Select.sql
@@ -18,3 +18,17 @@ SELECT CONCAT(first_name, ' ', last_name), email
 FROM "user"
 WHERE LOWER(email) LIKE CONCAT('%',LOWER(last_name),'%');
 
+--Фильмы, которые созданы режиссерами, моложе 60ти лет 
+--и с средним рейтингом пользователей больше восьми
+SELECT title AS Название, 
+	   CONCAT(first_name, ' ', last_name) AS Режиссер, 
+	   date_of_birth AS Год_рождения_режисера, 
+	   DATE_PART('year', AGE(DATE(NOW()), date_of_birth)) AS Возраст,
+	   ROUND(AVG(score), 1) AS Рейтинг
+FROM video
+JOIN shoot USING(id_video)
+JOIN directors USING(id_director)
+JOIN ratings USING(id_video)
+GROUP BY title, first_name, last_name, date_of_birth
+HAVING AVG(score) > 8 AND DATE_PART('year', AGE(DATE(NOW()), date_of_birth)) < 60
+ORDER BY Рейтинг DESC
